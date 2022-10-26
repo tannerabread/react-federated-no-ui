4d5ec084d990ccb05e687810a0fbcde3766c1b80
Bannon Tanner <bannonta@amazon.com>
2022-10-26 10:06:00 -0500

    federated amplify react app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Auth, Hub } from "aws-amplify";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
+      switch (event) {
+        case "signIn":
+        case "cognitoHostedUI":
+          getUser().then((userData) => setUser(userData));
+          break;
+        case "signOut":
+          setUser(null);
+          break;
+        case "signIn_failure":
+        case "cognitoHostedUI_failure":
+          console.log("Sign in failure", data);
+          break;
+        default:
+          break;
+      }
+    });
+
+    getUser().then((userData) => setUser(userData));
+  }, []);
+
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then((userData) => userData)
+      .catch(() => console.log("Not signed in"));
+  }
+
+  const username = "bannonta@amazon.com";
+  const password = "testtest";
+  async function signIn() {
+    try {
+      const user = await Auth.signIn(username, password);
+    } catch (error) {
+      console.log("error signing in", error);
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +51,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => Auth.federatedSignIn()}>Launch Hosted UI</button>
+        <button onClick={() => signIn()}>Sign In</button>
+        <button onClick={() => Auth.federatedSignIn({ provider: "Google" })}>
+          Launch Google
+        </button>
+        <button onClick={() => Auth.signOut()}>Sign Out</button>
+        <div>{JSON.stringify(user, null, 2)}</div>
       </header>
     </div>
   );
